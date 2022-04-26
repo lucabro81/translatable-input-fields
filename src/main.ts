10104a07f24527318d0bb698da0e7c97530b5d8f
Luca Brognara <luca.brognara@comperio.it>
2022-04-26 21:51:49 +0200

    minor fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
 import { wrap } from './utils';
 import observedAttributesTInput from './components/t-input/t-input.observed-attributes';
-import observedAttributesTestDev from './components/test-dev/test-dev.observed-attributes';
 customElements.define('t-input', wrap(()=>import('./components/t-input/t-input.component'), 'TInput', observedAttributesTInput));
-customElements.define('test-dev', wrap(()=>import('./components/test-dev/test-dev.component'), 'TestDev', observedAttributesTestDev));
